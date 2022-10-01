d56dfa35f0e30bf16679bb0358a56f8694885a68
Dmitrii Zverev <dima.newmail@gmail.com>
2022-10-02 08:52:27 +1000

    Initial Commit

---
+++ CatsOnSwiftUI/CatsOnSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsOnSwiftUIApp.swift
+//  CatsOnSwiftUI
+//
+//  Created by Dmitrii Zverev on 2/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct CatsOnSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
